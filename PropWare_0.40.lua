73c1283b14f6e00be6ec98ad58b99b13709cebdd
Sammy Folkhome <71086742+CatchySmile@users.noreply.github.com>
2024-03-28 13:16:45 -0300

    Update PropWare_0.41.lua

--- PropWare_0.40.lua
+++ PropWare_0.40.lua
@@ -1,13 +1,36 @@
 local frame = vgui.Create("DFrame")
 frame:SetSize(400, 260)
-frame:SetTitle("PropWare - v0.40")
+frame:SetTitle("PropWare - v0.41 Release")
 frame:SetVisible(true)
 frame:Center()
 
-
 frame.Paint = function(self, w, h)
-    draw.RoundedBox(8, 0, 0, w, h, Color(24, 24, 24, 200)) 
+    draw.RoundedBox(8, 0, 0, w, h, Color(24, 24, 24, 200))
+
+    surface.SetDrawColor(Color(0, 0, 0, 255))
+    surface.DrawOutlinedRect(0, 0, w, h)
+
+    local radius = 5
+    local corners = {
+        {x = 0, y = 0},
+        {x = w, y = 0},
+        {x = w, y = h},
+        {x = 0, y = h}
+    }
+
+    for _, corner in pairs(corners) do
+        draw.RoundedBox(radius, corner.x - radius, corner.y - radius, radius * 2, radius * 2, Color(0, 0, 0, 255))
+    end
 end
+
+
+
+-- If you remove my credits ill be oh so sad
+local labelModel = vgui.Create("DLabel", frame)
+labelModel:SetPos(327, 240)
+labelModel:SetSize(200, 20)
+labelModel:SetText("Made by 4koy")
+labelModel:SetTextColor(Color(255, 255, 255))
 
 
 local labelModel = vgui.Create("DLabel", frame)
@@ -34,7 +57,7 @@ local presetProps = {
     "models/hunter/tubes/tube4x4x025.mdl",
     "models/props_phx/construct/wood/wood_angle360.mdl",
     "models/props_phx/construct/windows/window2x2.mdl",
-    "models/props_c17/furnitureCouch002a.mdl",
+    "models/props_c17/gate_door02a.mdl",
     "models/balloons/hot_airballoon.mdl",
     "models/props_phx/oildrum001_explosive.mdl"
 }
@@ -257,7 +280,9 @@ hook.Add("PostDrawOpaqueRenderables", "DrawPlayerBoxesAndLines", function()
     local localPos = LocalPlayer():GetPos()
 
     local WireframeMaterial = Material("models/wireframe")
-    local lineColor = Color(255, 255, 255) 
+    local lineColor = Color(255, 255, 255)
+    local outlineColor = Color(0, 0, 0, 255) 
+    local outlineThickness = 1
 
     for _, ply in ipairs(players) do
         if IsValid(ply) and ply:IsPlayer() and ply:Alive() and ply ~= LocalPlayer() then 
@@ -265,13 +290,16 @@ hook.Add("PostDrawOpaqueRenderables", "DrawPlayerBoxesAndLines", function()
 
             if espEnabled then
                 cam.IgnoreZ(true)
-                render.SetMaterial(WireframeMaterial) 
-                render.DrawBox(plyPos, ply:GetAngles(), ply:OBBMins(), ply:OBBMaxs(), lineColor, true)
-                cam.IgnoreZ(false)
-            end
 
-            if espEnabled then
-                render.DrawLine(localPos, plyPos, lineColor, false)
+                render.SetMaterial(WireframeMaterial)
+                render.DrawBox(plyPos, ply:GetAngles(), ply:OBBMins() * 1.0, ply:OBBMaxs() * 1.0, outlineColor, true)
+
+
+                cam.IgnoreZ(false)
+
+                if espEnabled then
+                    render.DrawLine(localPos, plyPos, lineColor, false)
+                end
             end
         end
     end
@@ -294,54 +322,16 @@ end
 
 
 
-local distance = 130 
-local height = 30 
-local angleOffset = Angle(0, 0, 0) 
-local thirdPersonEnabled = false 
-
-hook.Add("CalcView", "ThirdPersonView", function(ply, origin, angles, fov)
-    local view = {}
-
-    if thirdPersonEnabled then
-        local forward = angles:Forward()
-        local right = angles:Right()
-        local up = angles:Up()
-
-        local cameraPos = origin - forward * distance + up * height
-
-        local cameraAngle = angles + angleOffset
-
-        view.origin = cameraPos
-        view.angles = cameraAngle
-        view.fov = fov
-        view.drawviewer = true
-
-        return view
-    end
-end)
 
 
 
-local thirdPersonCheckbox = vgui.Create("DCheckBoxLabel", frame)
-thirdPersonCheckbox:SetPos(10, 187)
-thirdPersonCheckbox:SetSize(200, 20)
-thirdPersonCheckbox:SetText("3rd Person")
-thirdPersonCheckbox:SetValue(thirdPersonEnabled)
-thirdPersonCheckbox:SizeToContents()
 
-thirdPersonCheckbox.OnChange = function(panel, isChecked)
-    thirdPersonEnabled = isChecked
-end
-
-
-
-local lookRandomCheckbox = vgui.Create("DCheckBoxLabel", frame)
+local lookRandomCheckbox = vgui.Create("DCheckBoxLabel", frame) 
 lookRandomCheckbox:SetPos(999, 999)
-lookRandomCheckbox:SetSize(0, 0)
 lookRandomCheckbox:SetValue(0)
 lookRandomCheckbox:SizeToContents()
 
-
+-- This shit is NOT silent
 local function LookRandomlyContinuously()
     if lookRandomCheckbox:GetChecked() then
         local yaw = math.random(0, 360)
@@ -370,6 +360,7 @@ spinCheckbox:SetValue(GetConVar("spin_enabled"):GetBool())
 spinCheckbox:SizeToContents()
 
 local spinSpeed = 700 
+-- Also not silent
 
 hook.Add("Think", "SpinCharacter", function()
     if GetConVar("spin_enabled"):GetBool() then
@@ -439,46 +430,233 @@ fovSlider:SetMax(150)
 fovSlider:SetDecimals(0)
 fovSlider:SetValue(110)
 
+local fovSliderIsValid = true
+
 local function ApplyFOV()
-    local newFOV = fovSlider:GetValue()
-    if LocalPlayer() then
+    if not freecamEnabled and LocalPlayer() then
+        local newFOV = fovSlider:GetValue()
         LocalPlayer():SetFOV(newFOV, 0.1)
     end
 end
 
-fovSlider.OnValueChanged = function(panel, value)
-    ApplyFOV()
-end
-
-local fovSliderIsValid = true 
-
-frame.OnClose = function()
-    hook.Remove("CalcView", "ChangeFOV")
-    fovSliderIsValid = false
-end
-
-hook.Add("CalcView", "ChangeFOV", function(ply, origin, angles, fov)
-    if fovSliderIsValid and ply == LocalPlayer() then
+local function CalcFOV(ply, origin, angles, fov)
+    if fovSliderIsValid and not freecamEnabled and ply == LocalPlayer() then
         return {
             fov = fovSlider:GetValue()
         }
     end
+end
+
+hook.Add("CalcView", "ChangeFOV", CalcFOV)
+
+
+
+local freecamCheckbox = vgui.Create("DCheckBoxLabel", frame)
+freecamCheckbox:SetPos(90, 207)
+freecamCheckbox:SetSize(200, 20)
+freecamCheckbox:SetText("Freecam")
+freecamCheckbox:SetValue(false)
+freecamCheckbox:SizeToContents()
+
+freecamCheckbox.OnChange = function(panel, isChecked)
+    if isChecked then
+        if input.IsKeyDown(KEY_LALT) then
+            freecamEnabled = false 
+        else
+            freecamAngles = LocalPlayer():EyeAngles()
+            freecamAngles2 = LocalPlayer():EyeAngles()
+            freecamPos = LocalPlayer():EyePos()
+            freecamEnabled = true 
+        end
+    else
+        freecamEnabled = false 
+    end
+end
+
+
+
+freecamAngles = Angle()
+freecamAngles2 = Angle()
+freecamPos = Vector()
+freecamEnabled = false
+freecamSpeed = 3
+keyPressed = false
+
+hook.Add("CreateMove", "lock_movement", function(ucmd)
+    if(freecamEnabled) then
+        ucmd:SetSideMove(0)
+        ucmd:SetForwardMove(0)
+        ucmd:SetViewAngles(freecamAngles2)
+        ucmd:RemoveKey(IN_JUMP)
+        ucmd:RemoveKey(IN_DUCK)
+        freecamAngles = (freecamAngles + Angle(ucmd:GetMouseY() * .023, ucmd:GetMouseX() * -.023, 0));
+        freecamAngles.p, freecamAngles.y, freecamAngles.x = math.Clamp(freecamAngles.p, -89, 89), math.NormalizeAngle(freecamAngles.y), math.NormalizeAngle(freecamAngles.x);
+        local curFreecamSpeed = freecamSpeed
+        if(input.IsKeyDown(KEY_LSHIFT)) then
+            curFreecamSpeed = freecamSpeed * 2
+        end
+        if(input.IsKeyDown(KEY_W)) then
+            freecamPos = freecamPos + (freecamAngles:Forward() * curFreecamSpeed)
+        end
+        if(input.IsKeyDown(KEY_S)) then
+            freecamPos = freecamPos - (freecamAngles:Forward() * curFreecamSpeed)
+        end
+        if(input.IsKeyDown(KEY_A)) then
+            freecamPos = freecamPos - (freecamAngles:Right() * curFreecamSpeed)
+        end
+        if(input.IsKeyDown(KEY_D)) then
+            freecamPos = freecamPos + (freecamAngles:Right() * curFreecamSpeed)
+        end
+        if(input.IsKeyDown(KEY_SPACE)) then
+            freecamPos = freecamPos + Vector(0,0,curFreecamSpeed)
+        end
+        if(input.IsKeyDown(KEY_LCONTROL)) then
+            freecamPos = freecamPos - Vector(0,0,curFreecamSpeed)
+        end
+    end
+end)
+hook.Add("CalcView", "freeCam", function(ply, pos, angles, fov)
+    local view = {}
+    if(freecamEnabled) then
+        view = {
+            origin = freecamPos,
+            angles = freecamAngles,
+            fov = fov,
+            drawviewer = true
+        }
+    else
+        view = {
+            origin = pos,
+            angles = angles,
+            fov = fov,
+            drawviewer = false
+        }
+    end
+	return view
 end)
 
 
+local propEspEnabled = false
+local selectedColorIndex = 1
+
+local colorOptions = {
+    Color(175, 94, 94, 70),
+    Color(255, 0, 0, 100),
+    Color(155, 155, 0, 100),
+    Color(0, 255, 0, 100),
+    Color(0, 155, 155, 100),
+    Color(0, 0, 255, 100), 
+    Color(155, 155, 0, 100),
+    Color(255, 255, 0, 100),
+    Color(155, 0, 155, 100),
+    Color(255, 0, 255, 100),
+    Color(0, 155, 155, 100), 
+    Color(0, 255, 255, 100)  
+}
+
+local function SetPropEspColor(colorIndex)
+    selectedColorIndex = colorIndex
+end
+
+local colorSlider = vgui.Create("DNumSlider", frame)
+colorSlider:SetPos(105, 183) 
+colorSlider:SetSize(180, 20) 
+colorSlider:SetText("")
+colorSlider:SetMin(1)
+colorSlider:SetMax(#colorOptions)
+colorSlider:SetDecimals(0)
+colorSlider:SetValue(selectedColorIndex)
+
+colorSlider.OnValueChanged = function(panel, value)
+    SetPropEspColor(math.floor(value + 0.5)) 
+end
+
+local function DrawPropESP()
+    if not propEspEnabled then return end
+    
+    local props = ents.FindByClass("prop_*") 
+    
+    cam.IgnoreZ(true)
+    
+    for _, prop in ipairs(props) do
+        if IsValid(prop) then
+            local propPos = prop:GetPos()
+            local propMins, propMaxs = prop:GetModelBounds()
+            local propAngles = prop:GetAngles()
+            
+            local forward = propAngles:Forward()
+            local right = propAngles:Right()
+            local up = propAngles:Up()
+            
+            local corners = {
+                propPos + forward * propMins.x + right * propMins.y + up * propMins.z,
+                propPos + forward * propMaxs.x + right * propMins.y + up * propMins.z,
+                propPos + forward * propMaxs.x + right * propMaxs.y + up * propMins.z,
+                propPos + forward * propMins.x + right * propMaxs.y + up * propMins.z,
+                propPos + forward * propMins.x + right * propMins.y + up * propMaxs.z,
+                propPos + forward * propMaxs.x + right * propMins.y + up * propMaxs.z,
+                propPos + forward * propMaxs.x + right * propMaxs.y + up * propMaxs.z,
+                propPos + forward * propMins.x + right * propMaxs.y + up * propMaxs.z
+            }
+            
+            render.SetMaterial(Material("color"))
+
+            render.DrawQuad(corners[1], corners[2], corners[3], corners[4], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[5], corners[6], corners[7], corners[8], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[1], corners[2], corners[6], corners[5], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[3], corners[4], corners[8], corners[7], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[1], corners[4], corners[8], corners[5], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[2], corners[3], corners[7], corners[6], colorOptions[selectedColorIndex])
+
+            render.DrawQuad(corners[4], corners[3], corners[2], corners[1], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[8], corners[7], corners[6], corners[5], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[5], corners[6], corners[2], corners[1], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[7], corners[8], corners[4], corners[3], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[5], corners[8], corners[7], corners[4], colorOptions[selectedColorIndex])
+            render.DrawQuad(corners[6], corners[7], corners[3], corners[2], colorOptions[selectedColorIndex])
+            
+            render.SetMaterial(Material("models/wireframe")) 
+            for i = 1, 4 do
+                render.DrawLine(corners[i], corners[i % 4 + 1], colorOptions[selectedColorIndex], true)
+                render.DrawLine(corners[i + 4], corners[i % 4 + 5], colorOptions[selectedColorIndex], true)
+                render.DrawLine(corners[i], corners[i + 4], colorOptions[selectedColorIndex], true)
+            end
+        end
+    end
+    
+    cam.IgnoreZ(false)
+end
 
 
-CreateClientConVar("crazyahh", 0, true, false)
+
+hook.Add("PostDrawOpaqueRenderables", "DrawPropESP", DrawPropESP)
+
+
+
+local propEspCheckbox = vgui.Create("DCheckBoxLabel", frame)
+propEspCheckbox:SetPos(170, 167)
+propEspCheckbox:SetSize(200, 20)
+propEspCheckbox:SetText("Prop Chams")
+propEspCheckbox:SetValue(propEspEnabled)
+propEspCheckbox:SizeToContents()
+
+propEspCheckbox.OnChange = function(panel, isChecked)
+    propEspEnabled = isChecked
+end
+
+
+
+CreateClientConVar("BhopVar", 0, true, false)
 
 local bunnyhopCheckbox = vgui.Create("DCheckBoxLabel", frame)
 bunnyhopCheckbox:SetPos(90, 187)
 bunnyhopCheckbox:SetSize(200, 20)
 bunnyhopCheckbox:SetText("Bhop")
-bunnyhopCheckbox:SetValue(GetConVar("crazyahh"):GetBool())
+bunnyhopCheckbox:SetValue(GetConVar("BhopVar"):GetBool())
 bunnyhopCheckbox:SizeToContents()
 
 local function Bunnyhop()
-    if GetConVar("crazyahh"):GetBool() then
+    if GetConVar("BhopVar"):GetBool() then
         if input.IsKeyDown(KEY_SPACE) then
             if LocalPlayer():IsOnGround() then
                 RunConsoleCommand("+jump")
@@ -494,6 +672,8 @@ local function Bunnyhop()
                     local addSpeed = math.max(0, 30 - speed)
                     local pushVel = forward * (addSpeed + 100) * 10
                     LocalPlayer():SetVelocity(velocity + pushVel)
+                else
+                    BhopVar = false 
                 end
             end
         end
@@ -504,16 +684,11 @@ hook.Add("Think", "Bunnyhop", Bunnyhop)
 
 bunnyhopCheckbox.OnChange = function(panel, isChecked)
     if isChecked then
-        RunConsoleCommand("crazyahh", "1")
+        RunConsoleCommand("BhopVar", "1")
     else
-        RunConsoleCommand("crazyahh", "0")
+        RunConsoleCommand("BhopVar", "0")
     end
 end
-
-
-
-
-
 
 
 frame:MakePopup()
@@ -526,7 +701,9 @@ local toggleKeys = {
     [KEY_INSERT] = "Insert",
     [KEY_HOME] = "Home",
     [KEY_DELETE] = "Delete",
-    [KEY_END] = "End"
+    [KEY_END] = "End",
+    [KEY_F10] = "F10",
+    [KEY_F9] = "F9"
 }
 
 local labelToggleGUI = vgui.Create("DLabel", frame)
@@ -551,14 +728,80 @@ toggleKeyComboBox.OnSelect = function(panel, index, value, data)
     toggleKey = data
 end
 
+
+
+local DetectPropFlingCheckbox = vgui.Create("DCheckBoxLabel", frame)
+DetectPropFlingCheckbox:SetPos(10, 187)
+DetectPropFlingCheckbox:SetSize(200, 20)
+DetectPropFlingCheckbox:SetText("Fling Alert")
+DetectPropFlingCheckbox:SetValue(0) 
+DetectPropFlingCheckbox:SizeToContents()
+
+local lastNotificationTime = 0
+local notificationDelay = 1.0 
+
+local function DetectPropFling()
+    local props = ents.FindByClass("prop_*")
+
+    for _, prop in ipairs(props) do
+        if IsValid(prop) then
+            local propPos = prop:GetPos()
+
+            for _, ply in ipairs(player.GetAll()) do
+                if IsValid(ply) then
+                    local plyPos = ply:GetPos()
+                    local distance = plyPos:Distance(propPos)
+
+                    if distance <= 1600 then 
+                        local velocity = prop:GetVelocity():Length() 
+                        local flingThreshold = 3640 
+
+                        if velocity > flingThreshold then
+                            local currentTime = CurTime()
+                            if currentTime - lastNotificationTime >= notificationDelay then
+                                notification.AddLegacy("Prop Fling Detected!", NOTIFY_GENERIC, 5)
+                                Msg("Prop Fling Detected!\n")
+                                lastNotificationTime = currentTime
+                            end
+                            break 
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+hook.Add("Think", "DetectPropFling", function()
+    if IsValid(DetectPropFlingCheckbox) and DetectPropFlingCheckbox:GetChecked() then
+        DetectPropFling()
+    end
+end)
+
+
 local function ToggleGUI()
     if IsValid(frame) then
         frame:SetVisible(not frame:IsVisible())
     else
-        print("If you see this, create an issue on the github!")
+        print("You must re-run the script because you hit the X")
     end
 end
 
+
+
+frame.OnClose = function()
+    hook.Remove("CalcView", "ChangeFOV")
+    fovSliderIsValid = false
+    DetectPropFlingCheckbox:SetValue(false)
+    espCheckbox:SetValue(false)
+    spinCheckbox:SetValue(false)
+    lightingModeCheckbox:SetValue(false)
+    bunnyhopCheckbox:SetValue(false)
+    propEspCheckbox:SetValue(false)
+    freecamCheckbox:SetValue(false)
+
+    propEspEnabled = false
+end
 
 
 hook.Add(
